7be6d55190431e0002ef047e0b0cbe4c5ffbd4c2
naibor <liznaibor.en@gmail.com>
2022-06-02 06:59:17 -0700

    update 4-puts file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry fuction
+ *
+ * puts: function that prints out a given string
+ * Return: returns 0 if successful and 1 if not
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
