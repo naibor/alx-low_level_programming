ae76fd94c4831be40924dc5e7c9f02d20733caab
naibor <liznaibor.en@gmail.com>
2022-06-02 13:00:27 -0700

    modify file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	char msg = "and that piece of art is useful\"
-                        - Dora Korpar, 2015-10-19\n"
+                        - Dora Korpar, 2015-10-19\n";
 	write(STDOUT_FILENO,msg,59);
 	return (1);
 }
