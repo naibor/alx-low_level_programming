6dec9e1c3699c8235f28d37a0779d9183b483beb
naibor <liznaibor.en@gmail.com>
2022-06-02 07:28:11 -0700

    update file 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the  program
+ *
+ * Description: A program that uses prinf to print out a string
+ * printf: A function that prints out a string
+ * Return: Always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
