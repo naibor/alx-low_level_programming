a0e97e3a59471d83675cf908d782fa129c2a7c55
naibor <liznaibor.en@gmail.com>
2022-06-03 15:18:48 -0700

    create and update file 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+
+	while (i < 10)
+	{
+		printf ("%d", i);
+		i++;
+	}
+	printf ("\n");
+
+	return (0);
+}
