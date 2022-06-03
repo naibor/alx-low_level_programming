db0135fc9ca56239e1db36d7b76024645856e7e2
naibor <liznaibor.en@gmail.com>
2022-06-03 08:56:42 -0700

    create and update 0-positive_or_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description:  Assigns a random number to variable n
+ *	 checks if number is positive, negative or zero
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if (n < 0) /*check if negative*/
+	{
+		printf("%d: is positive\n", n);
+	}
+	else if (n > 0) /*check if positive*/
+	{
+		printf("%d: is negative\n", n);
+	} else /*if zero*/
+	{
+		printf("%d: is zero\n", n);
+	}
+
+	return (0);
+}
