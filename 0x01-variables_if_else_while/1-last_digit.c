963038d83d1cdb29246a345f343a2f9704ad4952
naibor <liznaibor.en@gmail.com>
2022-06-03 09:41:21 -0700

    modify file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,13 +25,13 @@ int main(void)
 
 	if (l > 5) /*it's greater than 5*/
 	{
-		printf("Last digit of %d is %d and is greater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	} else if (l == 0) /*it's zero*/
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	} else if (l < 6 && l != 0) /*it's less than 6 and not 0*/
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 
 	return (0);
