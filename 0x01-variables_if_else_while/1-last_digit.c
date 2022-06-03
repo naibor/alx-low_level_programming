dc4a313d8474d2420f1f331f7d37973c3562797a
naibor <liznaibor.en@gmail.com>
2022-06-03 09:47:27 -0700

    modify file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,18 +19,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	/*last digit*/
-	int l;
-
-	l = n % 10; /*calculate last digit of n*/
-
-	if (l > 5) /*it's greater than 5*/
+	if (n % 10 > 5) /*it's greater than 5*/
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
-	} else if (l == 0) /*it's zero*/
+	} else if (n % 10 == 0) /*it's zero*/
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
-	} else if (l < 6 && l != 0) /*it's less than 6 and not 0*/
+	} else if (n % 10 < 6 && n % 10 != 0) /*it's less than 6 and not 0*/
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
