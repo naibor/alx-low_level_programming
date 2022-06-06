829fe6837d7a2480cf0d1a71c8f2cf668e577ed9
naibor <liznaibor.en@gmail.com>
2022-06-06 00:13:26 -0700

    create and update file 100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of two digits.
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+	int y;
+
+	for(i = '0' ; i < '9' ; i++)
+	{
+		 
+		for (y = i + 1 ; y <= '9' ; y++)
+		{
+			putchar(i);
+			putchar(y);
+			putchar(',');
+			putchar(' ');
+		}
+			
+	}
+	putchar('\n')	
+	return (0);
+}
