842c05f5c19cb26ea54e58d8693c81addbecc4da
naibor <liznaibor.en@gmail.com>
2022-06-06 16:37:23 -0700

    modify file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,13 +13,16 @@ int main(void)
 
 	for (i = 0 ; i < 98 ; i++)
 	{
-		for (y = i ; y <= 99 ; y++)
+		for (y = i + 1 ; y <= 99 ; y++)
 		{
 			putchar((i / 10) + '0');
+			/*the y%10 prints the last digit of y*/
 			putchar((y % 10) + '0');
 			putchar(' ');
 			putchar((i / 10) + '0');
 			putchar((y % 10) + '0');
+			if (i == 98 && y == 99)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
