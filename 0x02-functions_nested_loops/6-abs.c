a98295b0c41d6477a2d64560ec6e4ace550b2d41
naibor <liznaibor.en@gmail.com>
2022-06-08 02:25:40 +0300

    modify file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,16 +4,13 @@
  *
  * Description:  computes the absolute value of an integer
  *
+ * @n: parameter of type int, name n.
+ *
  * Return: returns 0
  */
 int _abs(int n)
 {
 	if (n >= 0)
-	{
 		return (n);
-	} else
-	{
-		return (n * -1);
-	}
-
+	return (n * -1);
 }
