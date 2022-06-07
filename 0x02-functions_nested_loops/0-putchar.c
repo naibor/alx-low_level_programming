dc272723ff6f598cf248dc86cd87ee3290e1d10b
naibor <liznaibor.en@gmail.com>
2022-06-07 11:17:50 -0700

    create required files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar, followed by a new line
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+	char b = 'a';
+	i = (int)b ;
+
+	printf("int value %d\n",i);
+	printf("char value %c\n", b);
+
+	return (0);
+
+
+}
