81e589549776abc6484c922f22629ef7e01c4292
naibor <liznaibor.en@gmail.com>
2022-06-07 12:57:10 -0700

    modify file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: returns 0
+ *
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	 print_alphabet(void);
+
+	 return (0);
+}
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 0; i ){}
+}
