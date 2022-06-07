0200bbdf86a73a745ae5b6655ea5009dad9ff5f0
naibor <liznaibor.en@gmail.com>
2022-06-08 01:33:32 +0300

    modify file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,19 +13,22 @@
  */
 int print_sign(int n)
 {
-	int result = 0;
+	char result = 0;
 
 	if (n < 0)
 	{
 		result = -1;
+		_putchar('-');
 
 	}else if (n > 0)
 	{
 		result = 1;
+		_putchar('+');
 	}
 	else
 	{
 		result = 0;
+		_putchar('0');
 	}
 	
 	return (result);
