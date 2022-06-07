5188be4e093f753575cbadbfb3ad7804afcdc52a
naibor <liznaibor.en@gmail.com>
2022-06-08 02:12:35 +0300

    update file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Function
+ *
+ * Description:  computes the absolute value of an integer
+ *
+ * Return: returns 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * (-1);
+	}
+
+	return (0);
+}
