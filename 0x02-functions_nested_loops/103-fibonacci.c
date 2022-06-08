20adb6283c98d413b7e43700f437659f5e8fc791
naibor <liznaibor.en@gmail.com>
2022-06-08 05:39:18 +0300

    modify file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	long int fib[50], sum = 2;
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (x = 2; x < 50; x++)
+	{
+		fib[x] = fib[x - 1] + fib[x - 2];
+		if ((fib[x] % 2) == 0 && fib[x] < 4000000)
+			sum += fib[x];
+	}
+	printf("%ld\n", sum);
+
+	return (0);
+}
