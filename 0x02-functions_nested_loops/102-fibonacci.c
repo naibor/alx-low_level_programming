34f8b252e05b3ac857ea4f454d66741a58a1d1a0
naibor <liznaibor.en@gmail.com>
2022-06-08 05:38:38 +0300

    modify file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,10 +2,9 @@
 
 /**
  * main - Entry point
- *  
+ *
  * Description: prints the first 50 Fibonacci numbers, starting with 1 and 2
  * followed by a new line
- * 
  * Return: returns 0 (Success)
  */
 int main(void)
