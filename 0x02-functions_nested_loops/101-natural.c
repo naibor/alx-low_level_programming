32321139ee190e4d8f765374f6082165726c6703
naibor <liznaibor.en@gmail.com>
2022-06-08 05:25:47 +0300

    modify file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,7 @@
 /**
  *main - Entry point
  *
- * Description: computes and prints the sum of all the multiples of 3 or 5 
+ * Description: computes and prints the sum of all the multiples of 3 or 5
  * below 1024 (excluded)
  *
  * Return: returns 0
