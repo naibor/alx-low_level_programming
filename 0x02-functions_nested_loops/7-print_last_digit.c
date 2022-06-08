f3331f36c84d0ddb8044043bfe67fcc599189525
naibor <liznaibor.en@gmail.com>
2022-06-08 03:04:05 +0300

    modify file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_last_digit - Function
+ *
+ * Description: prints the last digit of a number
+ *
+ * @n: parameter of type int, name n.
+ *
+ * Return: returns value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int l;
+
+	if(n < 0)
+	{
+		n = n * -1;
+		l = n % 10;
+		_putchar(l + '0');
+	} else
+	{
+		l = n % 10;
+		_putchar(l + '0');
+	}
+	return (l);
+
+}
