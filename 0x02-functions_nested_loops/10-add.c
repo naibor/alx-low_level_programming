3c3b170f1312a00b540cec61801b755352ea6dc0
naibor <liznaibor.en@gmail.com>
2022-06-08 03:40:33 +0300

    update file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Function
+ *
+ * Description: adds two integers and return a result
+ *
+ * Return: returns result of sum
+ */
+
+int add(int a, int b)
+{
+	return(a+b);
+}
