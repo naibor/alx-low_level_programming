689e1ae4184d5aea7aa35a8106b23bb3d229c716
naibor <liznaibor.en@gmail.com>
2022-06-08 05:08:29 +0300

    update file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * natural - Function
+ *
+ * Description: computes and prints the sum of all the multiples of 3 or 5 
+ * below 1024 (excluded)
+ *
+ *
+ *  
