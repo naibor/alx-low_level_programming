4011c1e16fa146f28ea90099bfe5dc439a8dd050
naibor <liznaibor.en@gmail.com>
2022-06-08 03:42:02 +0300

    modify file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,10 +4,13 @@
  *
  * Description: adds two integers and return a result
  *
+ * @a: parameter of type int, name a
+ * @b: parameter of type int, name b
+ *
  * Return: returns result of sum
  */
 
 int add(int a, int b)
 {
-	return(a+b);
+	return (a + b);
 }
