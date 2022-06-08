9ef3d7fba593bc99c7c694d95112e09714014b6d
naibor <liznaibor.en@gmail.com>
2022-06-08 21:49:59 +0300

    update files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - test function 
+ *
+ * Description: test function that prints if integer is positive or negative
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
