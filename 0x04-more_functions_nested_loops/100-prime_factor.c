04dd2a4c7bc7f60820affed0ac7755621886f30a
naibor <liznaibor.en@gmail.com>
2022-06-10 02:46:15 +0300

    update file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: finds and prints the largest prime factor
+ *  of the number 612852475143
+ *  followed by a new line
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+	long int x;
+	long int n = 612852475143;
+
+	for (x = 3 ; x < 12057; x += 2)
+	{
+		while (n % x == 0 && n != 0 )
+		{
+			n /= x;
+		}
+
+	}
+	printf("%ld\n", n);
+	return (0);
+}
