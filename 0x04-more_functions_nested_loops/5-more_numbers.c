8ddb674d252e3718d3210c8bde73d7da001aa50f
naibor <liznaibor.en@gmail.com>
2022-06-09 20:32:39 +0300

    modify file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 	int x;
 
 	y = 0;
-	while(y < 10)
+	while (y < 10)
 	{
-		for(x = 0; x <= 14; x++)
+		for (x = 0; x <= 14; x++)
 		{
-			if (x >9)
+			if (x > 9)
 			{
-				_putchar(x /10 + '0');
+				_putchar(x / 10 + '0');
 			}
 			_putchar(x % 10 + '0');
 		}
