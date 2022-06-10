f9672e6cb7296126c76fcba4e9b0dfb5f2686672
naibor <liznaibor.en@gmail.com>
2022-06-10 03:06:59 +0300

    update file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_number - Function
+ *
+ * Description: prints an integer.
+ *
+ * @n: parameter of type int,n
+ *
+ */
+void print_number(int n)
+{
+	if(n == 0)
+	{
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	else
+	{
+		_putchar(n%10 + '0');
+	}
+}
