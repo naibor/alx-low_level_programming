59a51cbc0488681df703ce755ceb8f3e95076a10
naibor <liznaibor.en@gmail.com>
2022-06-13 00:39:48 +0300

    modify files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,7 +21,13 @@ int main(void)
 			printf("FizzBuzz ");
 		} else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			if (i == 100)
+			{
+				printf("Buzz");
+			} else
+			{
+				printf("Buzz ");
+			}
 		} else if (i % 3 == 0)
 		{
 			printf("Fizz ");
