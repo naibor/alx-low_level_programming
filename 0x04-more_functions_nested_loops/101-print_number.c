f33031c25e93a59a0bef6588af81f6771ee324b8
naibor <liznaibor.en@gmail.com>
2022-06-13 01:10:28 +0300

    modify file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,17 +9,13 @@
  */
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	else
-	{
-		_putchar(n % 10 + '0');
-	}
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
 }
