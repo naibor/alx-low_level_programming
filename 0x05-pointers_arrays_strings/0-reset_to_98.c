d51fa94c382c1ec19cf6737976ea25c77f3a49b9
naibor <liznaibor.en@gmail.com>
2022-06-13 23:41:44 +0300

    modify file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,9 @@
  * reset_to_98 - Function
  *
  * Description: it takes a pointer to an int as parameter
- * updates the values it points to, to 98 
+ * updates the values it points to, to 98
  *
- * @*n: pointer parameter of type int, n
+ * @n: pointer parameter of type int, n
  *
  */
 void reset_to_98(int *n)
