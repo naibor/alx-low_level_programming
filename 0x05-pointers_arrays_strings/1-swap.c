807d207a48aa55666f650bf7a7856438010eacad
naibor <liznaibor.en@gmail.com>
2022-06-14 00:00:46 +0300

    modify file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 /**
  * swap_int - Function
  *
- * Description: swaps the values of two integers
- * 
+ * Description: swaps the values of two integer
+ *
  * @a: pointer parameter of type int, a to be swaped
  * @b: pointer parameter of type int, b to be swaped
  *
@@ -14,5 +14,5 @@ void swap_int(int *a, int *b)
 
 	c = *a;
 	*a = *b;
-	*b = c;	
+	*b = c;
 }
