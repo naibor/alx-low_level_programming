33fcd562493fa2f850197806cdef83a186903948
naibor <liznaibor.en@gmail.com>
2022-06-14 00:35:15 +0300

    update file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - Function
+ *
+ * Description: Gets the length of a string
+ *
+ * @s: pointer parameter of type char, s.
+ *
+ * Return: returns an int, the string length
+ */
+int _strlen(char *s)
+{
+	int x = 0;
+
+	for (x = 0; x <= s[x]; x++)
+	{
+	}
+
+	return (x);
+}
