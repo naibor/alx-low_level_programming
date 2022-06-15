b88b745e57487eef39ce5c74c6cd4ba45c5bf492
naibor <liznaibor.en@gmail.com>
2022-06-15 21:46:59 +0300

    update file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - Function
+ *
+ * Description:  concatenates two strings.
+ *
+ * @src: pointer parameter of type char, to be appended to dest
+ * @dest: pointer parameter of type char
+ *
+ * Return: returns resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ch = dest;
+	
+	while (*dest){
+		dest++;
+	}
+
+	while (*src){
+		*dest++ = *src++;
+	}
+
+	*dest = '\0';
+
+	return (ch);
+}
