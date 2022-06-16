0c30ca074822e97722bc6cb87de2b9e04c2e6d0b
naibor <liznaibor.en@gmail.com>
2022-06-16 15:16:15 +0300

    update file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * rot13 - Function
+ *
+ * Description: encodes a string using rot13
+ *
+ * @str: pointer parameter of type char, string input
+ *
+ * Return: returns char, resulting string
+ */
+char *rot13(char *str)
+{
+	char *ch = str;
+	int i, x;
+	char input[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	char output[53] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+
+	while (ch[i] != '\0')
+	{
+		for (x = 0; x < 53; x++)
+			if (ch[i] == input[x])
+			{
+				ch[i] = output[x];
+			}
+	i++;
+	}
+	return (ch);
+}
