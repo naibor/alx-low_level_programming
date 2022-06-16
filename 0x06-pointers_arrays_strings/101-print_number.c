15b57ce80ea7aae39bfd20c4f28f081534732780
naibor <liznaibor.en@gmail.com>
2022-06-17 01:18:10 +0300

    modify file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,7 +9,7 @@
  */
 void print_number(int n)
 {
-	int num = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
@@ -18,7 +18,7 @@ void print_number(int n)
 	}
 	else
 	{
-		num = num;
+		num;
 	}
 
 	if (num / 10 != 0)
