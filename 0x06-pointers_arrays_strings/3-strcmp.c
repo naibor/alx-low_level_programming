dd2116c726ebed90d9741348dd8174d8c8d90909
naibor <liznaibor.en@gmail.com>
2022-06-16 05:13:14 +0300

    update file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strcmp - Function
+ *
+ * Description: compares two strings.
+ *
+ * @s1: pointer parameter of type char
+ * @s2: pointer parameter of type char
+ *
+ * Return: returns +int when greater than,
+ * -int when less than, and 0 when greater.
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+	while (*s1 && *s2)
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++; 
+		}
+		else
+		{
+			return (*s1 - *s2);
+		}
+	}
+
+	return (*s1 - *s2);
+
+
+}
+
