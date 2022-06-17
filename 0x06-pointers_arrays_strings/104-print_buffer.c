d263be8521158fa840bdcac833aafd5c61706e60
naibor <liznaibor.en@gmail.com>
2022-06-17 22:37:51 +0300

    update file

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,57 @@
+#include "main.h"
+/**
+ * print_buffer - Function
+ *
+ * Description:  prints a buffer.
+ * print 10 bytes per line
+ * line starts with position of the first byte of the line
+ * in hexadecimal starting with 0
+ * line shows the hexadecimal content
+ * 2 char nof the buffer, 2bytes at a time
+ * end with a new line
+ *
+ * @b: pointer parameter of type char,
+ * @size: parameter of type int
+ *
+ */
+void print_buffer(char *b, int size)
+{
+	int byte, i;
+
+	for (byte = 0; byte < size; byte += 10)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + byte) >= size)
+				printf("  ");
+
+			else
+				printf("%02x", *(b + i + byte));
+
+			if ((i % 2) != 0 && i != 0)
+				printf(" ");
+		}
+
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + byte) >= size)
+				break;
+
+			else if (*(b + i + byte) >= 31 &&
+				 *(b + i + byte) <= 126)
+				printf("%c", *(b + i + byte));
+
+			else
+				printf(".");
+		}
+
+		if (byte >= size)
+			continue;
+
+		printf("\n");
+	}
+
+	if (size <= 0)
+		printf("\n");
+}
+
