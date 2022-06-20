9941cae0196e0b9f4ca330edf08ef090e536e52d
naibor <liznaibor.en@gmail.com>
2022-06-21 01:09:34 +0300

    modify file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,20 +13,27 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int bytes = 1;
 	char *reset = s;
+	int tag;
 
 	while (*accept)
 	{
+		tag = 0;
 		while (*s)
 		{
 			if (*s == *accept)
 			{
 				bytes++;
+				tag = 1;
 				break;
 			}
 			s++;
 		}
 		accept++;
 		s = reset;
+		if (tag == 0)
+		{
+			break;
+		}
 
 	}
 	return (bytes);
