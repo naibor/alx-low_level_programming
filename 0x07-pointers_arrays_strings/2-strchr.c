548dee8962ad8cee96c716a5472d26eac038ad32
naibor <liznaibor.en@gmail.com>
2022-06-20 23:12:19 +0300

    update file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strchr - Function
+ *
+ * Description: locates a character in a string
+ *
+ * @s: pointer parameter of type char, string to check
+ * @c: parameter of type int, char to chech for 
+ *
+ * Return: returns first occurance of char else Null
+ */
+char *_strchr(char *s, char c)
+{
+	/*char *ch = s;*/
+	int count = 0;
+
+	while(*s)
+	{
+
+		if ((*s == c) && (count = 0))
+		{		
+			return (s);
+			count++;
+		}
+		s++;
+	}
+
+		
+	return (NULL);
+
+}
