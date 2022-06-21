2558dbf4e616e4549db11884d5eb14f85b012741
naibor <liznaibor.en@gmail.com>
2022-06-21 05:32:54 +0300

    modify file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_string - Function
+ *
+ * Description: sets the value of a pointer to a char.
+ *
+ * @s: double pointer parameter of type char
+ * @to: pointer parameter of type char
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+
+}
