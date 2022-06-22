174f7c71c875d4b24bbcc4157d8b5a755b04a46b
naibor <liznaibor.en@gmail.com>
2022-06-23 00:21:50 +0300

    modify files

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * is_prime_number - Function
+ *
+ * Description: chaecks if a number is a prime number
+ *
+ * @n: parameter of type int
+ *
+ * Return: returns 1 if prime number else 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return prime(n, n -1);
+}
+
+/**
+ * prime - Function
+ *
+ * Description: calculates if num is prime
+ *
+ * @num: parameter of type int, our num
+ * @i: iterate
+ *
+ * Return: returns 0 if fails or 1 if success
+ */
+int prime(int num, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	if (num % i == 0 && i > 0)
+	{
+		return (0);
+	}
+	return (prime(num, i - 1));
+}
