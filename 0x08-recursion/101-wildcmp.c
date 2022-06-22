4b77bf6b3206701195c7efb8f0fb41b47c07a73d
naibor <liznaibor.en@gmail.com>
2022-06-23 01:00:47 +0300

    modify files

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * wildcmp - Function
+ *
+ * Description: compares two strings checks if they are identical
+ *
+ * @s1: Pointer parameter of type char
+ * @s2: pointer parameter of type char
+ *
+ * Return: returns 1 if success else 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+        {
+                return (1);
+        }
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+	{
+		return (0);
+	}
+
+	return (0);
+}
