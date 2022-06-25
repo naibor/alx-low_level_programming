b0a2070721d975c6853b7c5904548421715d6c8f
naibor <liznaibor.en@gmail.com>
2022-06-25 05:12:14 +0300

    modify file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -21,7 +21,39 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+		printf("%d\n", (_atoi(argv[1]) * _atoi(argv[2])));
 	}
 	return (0);
+}
+/**
+ * _atoi - Function
+ *
+ * Description: convert a string to an integer.
+ *
+ * @s: pointer parameter of type char ,s
+ *
+ * Return: returns 0 if no string
+ */
+int _atoi(char *s)
+{
+        int sign = 1;
+        unsigned int i = 0;
+
+        do {
+                if (*s == '-')
+                {
+                        sign *= -1;
+                }
+                else if (*s >= '0' && *s <= '9')
+                {
+                        i = (i * 10) + (*s - '0');
+                }
+                else if (i > 0)
+                {
+                        break;
+                }
+        } while (*s++);
+
+        return (i * sign);
+
 }
