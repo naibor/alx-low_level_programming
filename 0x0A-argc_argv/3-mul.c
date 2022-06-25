842a729e86630c630cbc19ed0255db0f887d99d9
naibor <liznaibor.en@gmail.com>
2022-06-25 05:14:46 +0300

    modify file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -36,24 +36,22 @@ int main(int argc, char *argv[])
  */
 int _atoi(char *s)
 {
-        int sign = 1;
-        unsigned int i = 0;
+	int sign = 1;
+	unsigned int i = 0;
 
-        do {
-                if (*s == '-')
-                {
-                        sign *= -1;
-                }
-                else if (*s >= '0' && *s <= '9')
-                {
-                        i = (i * 10) + (*s - '0');
-                }
-                else if (i > 0)
-                {
-                        break;
-                }
-        } while (*s++);
-
-        return (i * sign);
-
+	do {
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			i = (i * 10) + (*s - '0');
+		}
+		else if (i > 0)
+		{
+			break;
+		}
+	} while (*s++);
+	return (i * sign);
 }
