675cc5c35badbee7a32c758198d770a850efc1d1
naibor <liznaibor.en@gmail.com>
2022-06-25 04:55:43 +0300

    modify file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,14 +14,18 @@
  */
 int main(int argc, char *argv[])
 {
+	int x, y, mul;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
-	{			       	
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	{	x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		mul = x * y;		
+		printf("%d\n", mul);
 	}
 	return (0);
 }
