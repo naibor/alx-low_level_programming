bc7f5289f29d56544d26856efff5474ee90fe23d
naibor <liznaibor.en@gmail.com>
2022-06-29 04:50:01 +0300

    modify file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,44 +12,29 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str1 = s1, *str2 = s2, *both;
-	unsigned int len = 0, len2 = 0, length = 0, x;
+	unsigned int i = 0, length = 0, x;
 
-	while (*str1)
-	{
-		len++;
-		str1++;
-	}
 	if (str1 == NULL)
-	{
 		str1 = "";
-	}
-	while (*str2)
-	{
-		len2++;
-		str2++;
-	}
 	if (str2 == NULL)
-	{
 		str2 = "";
-	}
-	length = len + len2;
-	both = malloc(sizeof(char) * (length + 1));
-	str1 = both;
+
+	for (x = 0; str1[x] || str2[x]; x++)
+		length++;
+
+	length++;
+	both = malloc(sizeof(char) * (length));
+
 	if (both == NULL)
-	{
 		return (NULL);
-	}
-	for (x = 0; x < length; x++)
+
+	for (x = 0; str1[x]; x++)
 	{
-		if (x < len)
-		{
-			both[x] = str1[x];
-		}
-		else
-		{
-			both[x] = str2[x];
-		}
-		return (both);
+		both[i++] = str1[x];
+	}
+	for (x = 0; str2[x]; x++)
+	{
+		both[i++] = str2[x];
 	}
 
 	return (both);
