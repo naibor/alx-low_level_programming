4ef6eda13c3610f779adfd7d462ced1fcd0daaad
naibor <liznaibor.en@gmail.com>
2022-06-29 04:22:04 +0300

    update file

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * argstostr - Function
+ *
+ * Description: concatenates all the arguments of your program
+ *
+ * @ac: parameter of type int
+ * @av: pointer to a pointer of type char
+ *
+ * Return: returns NULL if any of the vals is 0 and char otherwise
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str = "";
+	int i, x, length, y = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (x = 0; av[i][x] != '\0'; x++)
+		{
+			length++;
+		}
+		length++;
+	}
+	length++;
+	str = malloc(length * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (x = 0; av[i][x] != '\0'; x++)
+		{
+			str[y] = av[i][x];
+			y++;
+		}
+		str[y] = '\n';
+		y++;
+	}
+	str[y] = '\n';
+	return (str);
+
+}
