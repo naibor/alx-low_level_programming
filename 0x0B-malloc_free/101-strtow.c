e5ef75c60cd47bf4c8b71bdfe6beeb63d003e3ac
naibor <liznaibor.en@gmail.com>
2022-06-29 05:09:02 +0300

    update file

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * strtow - Function
+ *
+ * Description: splits a string into words
+ *
+ * @str: parameter of type char, the string to split
+ *
+ * Return: returns NULL if string is empty
+ */
+char **strtow(char *str)
+{
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+
+
+}
