4c7c472c3b120c4661202d732cfdeb272a92f2c4
naibor <liznaibor.en@gmail.com>
2022-07-05 02:59:46 +0300

    update file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - Function
+ *
+ * Description: prints a struct dog
+ *
+ * @d: pointer parameter of type dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+	{
+		d->name = "nil";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "nil";
+	}
+	if (d != NULL)
+	{
+		printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - new type called dog
  * @name: pointer member of type char,name of dog
@@ -15,4 +16,5 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
