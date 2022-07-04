ecbdff2f87a8c76156be72ad07ba9a2b240d1bcb
naibor <liznaibor.en@gmail.com>
2022-07-05 02:06:32 +0300

    update files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new type called dog
+ * @name: pointer member of type char,name of dog
+ * @age: member of type float, age of dog
+ * @owner: pointer member of type char, owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
