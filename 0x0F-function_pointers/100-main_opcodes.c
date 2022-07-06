63c7a562840bf2f8993e8968b6565d8c3cee8866
naibor <liznaibor.en@gmail.com>
2022-07-07 01:53:12 +0300

    create file

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints opcode of main function
+ *
+ * @argv: pointer parameter, arguments passed
+ * @argc: parameter of type int, number of arguments
+ *
+ * Return: returns 1 and 2 if fail and 0 on success
+ */
+int main (int argc, char *argv[])
+{
+
+}
