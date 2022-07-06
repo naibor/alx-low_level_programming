4538b8ea73ce80434cc400883cce74e0ff133900
naibor <liznaibor.en@gmail.com>
2022-07-07 00:51:22 +0300

    update files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ * main - Function entry point
+ *
+ * @argc: number of arguments
+ * @argv: arguments passed to the function
+ *
+ * Return: returns int
+ */
+int main(int argc, char *argv[])
+{
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	f = get_op_func(argv[2]);
+
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,87 @@
+#include "3-calc.h"
+/**
+ * op_add - Function
+ *
+ * Description: calculates sum of two values
+ *
+ * @a: parameter of type int, one of the values
+ * @b: parameter of type in, the other value
+ *
+ * Return: returns int, sum of numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+
+}
+
+/**
+ * op_sub - Function
+ *
+ * Description: calculates difference of two values
+ *
+ * @a: parameter of type int, one of the values
+ * @b: parameter of type in, the other value
+ *
+ * Return: returns int, difference of numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function
+ *
+ * Description: calculates product of two values
+ *
+ * @a: parameter of type int, one of the values
+ * @b: parameter of type in, the other value
+ *
+ * Return: returns int, product of numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function
+ *
+ * Description: calculates division of two values
+ *
+ * @a: parameter of type int, one of the values
+ * @b: parameter of type in, the other value
+ *
+ * Return: returns int, division of numbers
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - Function
+ *
+ * Description: calculates remainder of two values divided
+ *
+ * @a: parameter of type int, one of the values
+ * @b: parameter of type in, the other value
+ *
+ * Return: returns int, remainder of numbers
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (a % b);
+}
+
