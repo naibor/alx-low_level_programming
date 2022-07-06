63e5ac94d3563e0559d742f0427765fd37e1910e
naibor <liznaibor.en@gmail.com>
2022-07-06 20:45:38 +0300

    update files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - Function
+ *
+ * Description: prints a name
+ *
+ * @name: pointer parameter of type char, name to be printed
+ * @f: a pointer function, that returns void
+ * with pointer parameter of type char
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
