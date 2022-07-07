2ceef29357070688b9e2b008ef7adae69cbcbf48
naibor <liznaibor.en@gmail.com>
2022-07-08 01:45:17 +0300

    modify files

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,30 +5,21 @@
  * Description: prints numbers followed by a new line
  *
  * @separator: constant pointer parameter of type char
- * @n: constant parameter of type int
+ * @n: constant parameter of type unsigned int
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list numbers;
-	int i = n;
+	unsigned int i;
 
-	if (separator != NULL)
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(numbers, n);
-		while (i != 0)
-		{
-			if (i == 1)
-				printf("%d", va_arg(numbers, int));
-			else
-			{
-				printf("%d", va_arg(numbers, int));
-				printf("%s", separator);
-			}
-			i--;
-
-		}
-		printf("\n");
-		va_end(numbers);
+		printf("%d", va_arg(numbers, int));
+		
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
-
+	printf("\n");
+	va_end(numbers);
 }
