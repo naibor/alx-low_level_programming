eb547e7669576d13c824688235fce065834fed8d
naibor <liznaibor.en@gmail.com>
2022-07-15 01:00:27 +0300

    update file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - Function
+ *
+ * Description: prints all the elements of a list_t list
+ *
+ * @h: constant pointer parameter of type list_t
+ *
+ * Return: returns number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[%u] %s\n", 0, "(nill)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		/*increments length count*/
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
