ca70eeacf0bce3daa3b1341f21558bec3b1b1db2
naibor <liznaibor.en@gmail.com>
2022-07-26 02:52:38 +0300

    update file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: copies the content of a file to another file
+ *
+ * @argc: The number of arguments
+ * @argv: The pointer to array of arguments
+ *
+ * Return: returns int
+ */
+int main(int argc, char *argv[])
+{
+	return (1);
+}
